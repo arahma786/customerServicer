8a70fc8ae4daeb1e2856bc87b08df96340f50b75
arahma <arahma@iu.edu>
2023-04-12 18:25:52 -0400

    .

--- trackingservice/src/main/java/edu/iu/c322/trackingservice/repository/TrackingInfoRepository.java
+++ trackingservice/src/main/java/edu/iu/c322/trackingservice/repository/TrackingInfoRepository.java
@@ -4,6 +4,6 @@ import edu.iu.c322.trackingservice.model.Tracking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public class TrackingInfoRepository extends JpaRepository<Tracking, Long>{
+public interface TrackingInfoRepository extends JpaRepository<Tracking, Long>{
     List<Tracking> findByOrderId(String orderId);
 }
